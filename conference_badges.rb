9c6cf7ae5564e3cc9d79941fda62e4e782630fac
Leelena Asfaw <leelena.asfaw@gmail.com>
2018-09-30 21:39:36 +0000

    automatically backed up by learn

---
+++ conference_badges.rb
@@ -0,0 +1,29 @@
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+speakers= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", and "Matz"]
+
+def batch_badge_creator(speakers)
+  speakers.collect do |name|
+    badge_maker(name)
+  end
+
+end
+
+def assign_rooms(speakers)
+  room=0
+  speakers.collect do |name|
+    room+=1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |value|
+    puts value
+  end
+  assign_rooms(speakers).each do |value|
+    puts value
+  end
+end
---
+++ playground.rb
@@ -0,0 +1,25 @@
+# def my_method(cars)
+#   cars.each do |car|
+#     if car == "Range Rover"
+#       puts "My favorite car is #{car}"
+#     else 
+#       puts car
+#     end
+#   end
+# end
+
+
+
+my_array=["Toyota","Honda","Range Rover"]
+
+
+def loop_over_array(my_array)
+  counter = 0
+  while counter < array.length
+  puts array[counter]
+  counter += 1
+end
+  
+  
+end
+loop_over_array(my_array)
